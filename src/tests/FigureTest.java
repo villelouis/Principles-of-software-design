4e315b76f8fbd2f5b279941641c4d13fdfb1626e
Maxim Yakimchikov <villelouis32@gmail.com>
2018-09-25 18:54:16 +0300

    Figures

--- src/tests/FigureTest.java
+++ src/tests/FigureTest.java
@@ -11,17 +11,17 @@ class FigureTest {
     private static final double  RADIUS = 10;
     private static final double  WIDTH = 10;
     private static final double  HEIGHT = 10;
-    private static final double  SIDE_LENGHT = 10;
-
+    private static final double  SIDE_LENGTH = 10;
+    
     private Figure circle = new Circle(RADIUS);
     private Figure rectangle = new Rectangle(WIDTH,HEIGHT);
-    private Figure square = new Square(SIDE_LENGHT);
+    private Figure square = new Square(SIDE_LENGTH);
 
     @Test
     void calculateArea() {
         Assert.assertEquals(Math.PI*Math.pow(RADIUS,2),circle.calculateArea(),0.01);
         Assert.assertEquals(WIDTH*HEIGHT,rectangle.calculateArea(),0.01);
-        Assert.assertEquals(Math.pow(SIDE_LENGHT,2),square.calculateArea(),0.01);
+        Assert.assertEquals(Math.pow(SIDE_LENGTH,2),square.calculateArea(),0.01);
     }
 
 }
